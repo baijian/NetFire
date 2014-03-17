0e9f7fdfb65ec9ed128e1ffba23ec34f64d76a38
柏健 <jian.baij@gmail.com>
2014-03-17 17:22:26 +0800

    init project

---
+++ NetFire/ShootAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ShootAppDelegate.h
+//  NetFire
+//
+//  Created by baijian on 3/17/14.
+//  Copyright (c) 2014 baijian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShootAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NetFire/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NetFire
+//
+//  Created by baijian on 3/17/14.
+//  Copyright (c) 2014 baijian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ShootAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ShootAppDelegate class]));
+    }
+}
